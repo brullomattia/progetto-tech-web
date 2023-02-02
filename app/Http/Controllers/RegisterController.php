672c8ade4313f5cac1b927089e4ea1a64c8e5398
MarcoBoo <96247662+MarcoBoo@users.noreply.github.com>
2023-02-02 13:12:19 +0100

    fix: cloud database

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,19 +15,21 @@ class RegisterController extends Controller
         //create the user
         $attributes = request()->validate([
             'username' => 'required|min:5|max:255|unique:users',
+           //'username' => 'required|unique:users',
             'password' => 'required|max:255',
             'email' => 'required|email|min:7|max:255|unique:users'
         ]);
 
         //$attributes['password'] = bcrypt($attributes['password']);
+
         $user= User::create($attributes);
-        
-        //log the user 
+        //dd($user);
+        //log the user
         auth()->login($user);
 
         return redirect('/')->with('success', 'Il tuo account è stato creato correttamente!');
-       
+
     }
 
-    
+
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Model::unguard();
+        /*Model::unguard();*/
+        Schema::defaultStringLength(191);
     }
 }
